f5c5d8d9bccb4bae6be5e5aa8bf92fc59bbef872
albcmnd <blue3agl3@gmail.com>
2026-03-07 02:50:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,38 @@ const p=document.createElement("div")
 
 p.classList.add("particle")
 
+p.style.position="fixed"
+p.style.width="2px"
+p.style.height="2px"
+p.style.background="red"
+
 p.style.left=Math.random()*100+"vw"
-p.style.animationDuration=5+Math.random()*10+"s"
+p.style.top=Math.random()*100+"vh"
+
 p.style.opacity=Math.random()
+
+p.style.animation="float "+(5+Math.random()*10)+"s linear infinite"
 
 particles.appendChild(p)
 
 }
+
+const style=document.createElement("style")
+
+style.innerHTML=`
+
+@keyframes float{
+
+from{
+transform:translateY(0px);
+}
+
+to{
+transform:translateY(-100vh);
+}
+
+}
+
+`
+
+document.head.appendChild(style)
